399915d5b7cc43b34617afddd524f7d5c5eef8c6
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-15 23:29:53 -0300

    Added primeros 3 de la guia de prolog

---
+++ Prolog/1.pl
@@ -0,0 +1,17 @@
+padre(juan, carlos).
+padre(juan, luis).
+padre(carlos, daniel).
+padre(carlos, diego).
+padre(luis, pablo).
+padre(luis, manuel).
+padre(luis, ramiro).
+abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
+hijo(X,Y) :- padre(Y,X).
+hermano(X,Y) :- padre(Z,X), padre(Z,Y), X \= Y.
+descendiente(X,Y) :- padre(Y,X).
+descendiente(X,Y) :- padre(Z,X), descendiente(Z,Y). 
+ancestro(X, Y) :- padre(X,Y).
+ancestro(X, Y) :- padre(Z,Y), ancestro(X,Z).
+
+
+
