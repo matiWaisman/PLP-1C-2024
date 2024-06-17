af3c886afef71a6524969a6182b3b9b245df5886
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-17 20:02:52 -0300

    Edit ejercicio 1

--- Prolog/1.pl
+++ Prolog/1.pl
@@ -5,11 +5,16 @@ padre(carlos, diego).
 padre(luis, pablo).
 padre(luis, manuel).
 padre(luis, ramiro).
+
 abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
+
 hijo(X,Y) :- padre(Y,X).
+
 hermano(X,Y) :- padre(Z,X), padre(Z,Y), X \= Y.
+
 descendiente(X,Y) :- padre(Y,X).
 descendiente(X,Y) :- padre(Z,X), descendiente(Z,Y). 
+
 ancestro(X, Y) :- padre(X,Y).
 ancestro(X, Y) :- padre(Z,Y), ancestro(X,Z).
 
