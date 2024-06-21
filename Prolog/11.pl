1b12b71b1190263b8b71c6dad932992e538c7cdd
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-21 17:25:35 -0300

    Added patrones instanciacion ejercicio 11

--- Prolog/11.pl
+++ Prolog/11.pl
@@ -1,4 +1,8 @@
-vacio([]).
+% vacío([])
+% intercalar(+L1, +L2, -L3)
 intercalar(L1, [], L1).
-intercalar([], L2, L2) :- not(vacio(L2)). % Para que no tire repetidos
+intercalar([], L2, L2) :- \+ vacío(L2). % Para que no tire repetidos
 intercalar([X|L1], [Y|L2], L3) :- append([X,Y|[]], LI, L3), intercalar(L1, L2, LI).
+
+% Se puede obtener L3 de L1 y L2 instanciados, se puede obtener L1 y L2 de solo L3 instanciados (también se puede tener L1 o L2 instanciados con L3 y obtener el restante).
+% También se puede determinar si dadas las 3 instanciadas es verdadero o falso que L3 es la intercalación de L1 y L2.
