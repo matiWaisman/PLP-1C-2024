485ec5388c6ff96077585da8ed1a1c7a2e52fc97
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-21 19:45:41 -0300

    Terminado ejercicio 15

--- Prolog/15.pl
+++ Prolog/15.pl
@@ -25,3 +25,28 @@ desde2(X,Y) :- var(Y), N is X + 1, desde2(N,Y).
 
 % cuadradoSemiLatino(+N, -XS).
 cuadradoSemiLatino(N, XS) :- desde2(0, X), armarMatriz(N, N, X, XS).
+
+% cuadradoMagico(+N, -XS), que instancia XS con cuadrados cuyas filas y columnas suman todas un mismo valor.
+
+% iésimo(?I, +L, -X), donde X es el I-ésimo elemento de la lista L. 
+iesimo(1, [X|_], X).
+iesimo(I, [_|L], X) :- iesimo(J, L, X), I is J + 1.
+
+% Devuelve la lista de la columna de una matriz
+% columna(+C, +Filas, +Matriz, -L)
+columna(C, 1, Matriz, [X]) :- iesimo(1, Matriz, L), iesimo(C, L, X).
+columna(C, Filas, Matriz, L) :- Filas > 1, iesimo(Filas, Matriz, Fila), iesimo(C, Fila, X), F is Filas - 1,
+                                columna(C, F, Matriz, CasiLista), append([X], CasiLista, L).
+
+% columnasSuman(+Columnas, +Target, +Matriz)
+columnasSuman(1, Target, Matriz) :- longitud(Matriz, L), columna(1, L, Matriz, Columna), 
+                                    sumlist(Columna, Sum), Sum =:= Target.
+columnasSuman(Columnas, Target, Matriz) :- Columnas > 1, longitud(Matriz, L), columna(Columnas, L, Matriz, Columna), 
+                                           sumlist(Columna, Sum), Sum =:= Target, 
+                                           C is Columnas - 1, columnasSuman(C, Target, Matriz).
+
+% filaSuma(+Matriz, -S) 
+filaSuma(Matriz, S) :- iesimo(1, Matriz, Fila), sumlist(Fila, S).
+
+% cuadradoMagico(+N, -XS)
+cuadradoMagico(N, XS) :- cuadradoSemiLatino(N, XS), filaSuma(XS, S), columnasSuman(N, S, XS). 
