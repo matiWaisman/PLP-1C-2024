1dce14a4c2d048ae539524c888621bdf4f93b84c
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-21 22:52:16 -0300

    Added idea ejercicio 20

---
+++ Prolog/20.pl
@@ -0,0 +1,13 @@
+% Dado un predicado unario P sobre números naturales, definir un predicado que determine el mínimo X que satisfaga P(X).
+
+% Sup que ese predicado unario es esPar: 
+% esPar(?X)
+esPar(X) :- desde2(0, X), X mod 2 =:= 0.
+
+% desde2(+X,?Y)
+desde2(X, X).
+desde2(X,Y) :- nonvar(Y), X < Y.
+desde2(X,Y) :- var(Y), N is X + 1, desde2(N,Y).
+
+% minimo(?X)
+minimo(X) :- esPar(X), not((esPar(Y), Y < X)).
