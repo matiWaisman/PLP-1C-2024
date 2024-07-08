c1e9a7ab44779d5a1e39779de29132d7f8483075
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-07-08 19:10:20 -0300

    Added archivo para hacer ejercicios de las teoricas de Haskell

---
+++ repasoFinal.hs
@@ -0,0 +1,25 @@
+merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ l1 [] = l1
+merge _ [] l2 = l2
+merge p (e1 : xs1) (e2 : xs2) = if p e1 e2 then e1 : merge p xs1 (e2 : xs2) else e2 : merge p (e1 : xs1) xs2
+
+mergesort :: (a -> a -> Bool) -> [a] -> [a]
+mergesort _ [e] = [e]
+mergesort p l = merge p (mergesort p primeraLista) (mergesort p segundaLista)
+    where primeraLista = take (length l `div` 2) l
+          segundaLista = drop (length l `div` 2) l
+
+zipE :: [a] -> [b] -> [(a, b)]
+zipE [] [] = []
+zipE (x : xs) (y : ys) = (x, y) : zipE xs ys
+
+-- Definir foldr en base a recr
+recr f z [] = z
+recr f z (x : xs) = f x xs (recr f z xs)
+
+foldrRec :: (a -> b -> b) -> b -> [a] -> b
+foldrRec f = recr (\x _ rec -> f x rec)
+
+main :: IO ()
+main = do
+    print (" Hola PLP")
