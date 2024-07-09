f05c8e4c7430c8dff2b2e11f57c18da4025d4a9e
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-07-09 14:37:33 -0300

    Added ejercicios teoricas para repasar haskell

--- repasoFinal.hs
+++ repasoFinal.hs
@@ -14,14 +14,41 @@ zipE [] [] = []
 zipE (x : xs) (y : ys) = (x, y) : zipE xs ys
 
 -- Definir foldr en base a recr
+recr :: (a -> [a] -> b -> b) -> b -> [a] -> b
 recr f z [] = z
 recr f z (x : xs) = f x xs (recr f z xs)
 
 foldrRecr :: (a -> b -> b) -> b -> [a] -> b
 foldrRecr f = recr (\x _ rec -> f x rec)
 
-recrFoldr :: 
+-- Definir recr en base a foldr
+recrFoldr :: (a -> [a] -> b -> b) -> b -> [a] -> b
+recrFoldr f z (x : y:  xs) = f x xs (foldr (\_ rec -> f y xs rec) z xs)
+
+recFoldrMalPorqueLoHizoFausto :: (a -> [a] -> b -> b) -> b -> [a] -> b
+recFoldrMalPorqueLoHizoFausto f z xs = foldr (\x rec -> f x xs rec ) z xs
+
+-- Prueba de que funciona bien 
+trimP :: String -> String
+trimP = recFoldrMalPorqueLoHizoFausto (\ x xs rec -> if x == ' ' then rec else x : xs) []
+
+-- Definir foldl en base a foldr
+foldlFoldr :: (b -> a -> b) -> b -> [a] -> b
+foldlFoldr f rec l = foldr g rec (reverse l)
+    where g e rec = f rec e
+
+-- Prueba de que funciona bien 
+bin2dec :: [Int] -> Int
+bin2dec = foldlFoldr (\ ac b -> b + 2 * ac) 0
+
+-- Definir foldr en base a foldl
+foldrFoldl :: (a -> b -> b) -> b -> [a] -> b
+foldrFoldl f rec l = foldl g rec (reverse l)
+    where g rec e = f e rec
+
+entrelazar :: [a] -> [a] -> [a]
+entrelazar xs ys = foldr (\x rec -> if null ys then x : rec else x : head ys : rec) (ys) xs
 
 main :: IO ()
 main = do
-    print (" Hola PLP")
+    print (entrelazar [1, 3, 5] [2, 4, 6])
